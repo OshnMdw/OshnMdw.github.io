1ee8f51dcea47bf80773972f1ef486c8b8f07d8e
OshnMdw <gchoshanmadawa@gmail.com>
2023-09-26 12:56:04 +0530

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/myprofile.png'
+import HeaderSocial from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h3>Hello I'm</h3>
+        <h1>Oshan Madawa</h1>
+        <h3 className='text-light'>Fullstack Developer</h3>
+        <CTA/>
+        <HeaderSocial/>
+
+        <div className='me'>
+          <img src={ME} alt=""/>
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './projects.css'
+
+const Projects = () => {
+  return (
+    <section id='projects'>
+      <h5>My Recent Works</h5>
+      <h2>Projects</h2>
+      
+    </section>
+  )
+}
+
+export default Projects
