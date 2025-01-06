ea207e6806b42b0c88b90105af8699bc6381e179
OshnMdw <gchoshanmadawa@gmail.com>
2025-01-06 22:40:52 +0530

    New changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,57 +1,82 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import './contact.css'
-import {HiOutlineMail} from 'react-icons/hi'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_ym87pm8', 'template_ra9z4nb', form.current, 'gUTbAWndtq3BhMqnB')
+    emailjs.sendForm(
+      "service_ym87pm8",
+      "template_ra9z4nb",
+      form.current,
+      "gUTbAWndtq3BhMqnB"
+    );
 
-    e.target.reset()
+    e.target.reset();
   };
 
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
-        <div className='contact__options'>
-          <article className='contact__option'>
-            <HiOutlineMail className='contact__option-icon'/>
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>gchoshanmadawa@gmail.com</h5>
-            <a href="mailto:<EMAIL>" target="_blank" >Send a message</a>
+            <h5>oshanm.20@cse.mrt.ac.lk</h5>
+            <a href="mailto:oshanm.20@cse.mrt.ac.lk" target="_blank">
+              Send a message
+            </a>
           </article>
-          <article className='contact__option'>
-            <RiMessengerLine className='contact__option-icon'/>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Oshan Madawa</h5>
-            <a href="https://m.me/oshan.madawa.758" target="_blank" >Send a message</a>
+            <a href="https://m.me/oshan.madawa.758" target="_blank">
+              Send a message
+            </a>
           </article>
-          <article className='contact__option'>
-            <BsWhatsapp className='contact__option-icon'/>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+94765949000</h5>
-            <a href="https://api.whatsapp.com/send?phone=+94765949000" target="_blank" >Send a message</a>
+            <a
+              href="https://api.whatsapp.com/send?phone=+94765949000"
+              target="_blank"
+            >
+              Send a message
+            </a>
           </article>
         </div>
         {/*END OF CONTACT OPTION*/}
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required/>
-          <input type='email' name = 'email' placeholder='Your Email' required/>
-          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import './footer.css'
-import {BsInstagram,BsLinkedin,BsGithub} from 'react-icons/bs'
+import React from "react";
+import "./footer.css";
+import { BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      
-      <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
-      <div className='footer__socials'>
-        <a href='https://www.linkedin.com/in/oshnmdw/' target="_blank"><BsLinkedin/></a>
-        <a href='https://github.com/OshnMdw' target="_blank"><BsGithub/></a>
-        <a href="https://www.instagram.com/oshnmdw/" target="_blank"><BsInstagram/></a>
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/oshnmdw/" target="_blank">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/OshnMdw" target="_blank">
+          <BsGithub />
+        </a>
+        <a href="https://www.instagram.com/oshnmdw/" target="_blank">
+          <BsInstagram />
+        </a>
       </div>
 
-      <div className='footer__copyright'>
-        <small>&copy;2023 All rights reserved.</small>
+      <div className="footer__copyright">
+        <small>&copy;2025 All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
