cd26a974566fc1764a7aba1557732270eb9d1009
OshnMdw <gchoshanmadawa@gmail.com>
2025-01-16 18:33:45 +0530

    Changed the navbar by adding past job section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,61 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser } from'react-icons/ai'
-import { BiBook} from "react-icons/bi"
-import { VscFileSubmodule} from'react-icons/vsc'
-import { BiMessageSquareDetail  } from'react-icons/bi'
-import { useState } from 'react'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { VscFileSubmodule } from "react-icons/vsc";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { ImOffice } from "react-icons/im";
+import { useState } from "react";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#');
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active':''}><AiOutlineHome/></a>
-      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active':''}><AiOutlineUser/></a>
-      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active':''}><BiBook/></a>
-      <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active':''}><VscFileSubmodule/></a>
-      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active':''}><BiMessageSquareDetail/></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#pastJobs"
+        onClick={() => setActiveNav("#pastJobs")}
+        className={activeNav === "#pastJobs" ? "active" : ""}
+      >
+        <ImOffice />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#projects"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
+        <VscFileSubmodule />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
