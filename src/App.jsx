7c07161df1f93afeb6011c5cc6cd63e3c5028682
OshnMdw <gchoshanmadawa@gmail.com>
2025-01-16 18:31:55 +0530

    Added routings

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import PastJobs from "./components/pastJobs/PastJobs";
 
 const App = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <Nav />
       <About />
+      <PastJobs />
       <Experience />
       <Projects />
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
