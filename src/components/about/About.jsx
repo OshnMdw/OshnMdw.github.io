00152b13df3475a7f099bee8640150d3699ec292
OshnMdw <gchoshanmadawa@gmail.com>
2025-01-16 18:38:19 +0530

    Update About me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,59 +1,56 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/aboutmenew.png'
-import {FaAward} from 'react-icons/fa'
-import {LiaLanguageSolid} from 'react-icons/lia'
-import {LuFolderTree} from 'react-icons/lu'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/aboutmenew.png";
+import { FaAward } from "react-icons/fa";
+import { LiaLanguageSolid } from "react-icons/lia";
+import { LuFolderTree } from "react-icons/lu";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get to Know</h5>
       <h2>About Me</h2>
 
-      <div className='container about__container'>
-
-        <div className='about__me'>
-          <div className='about__me-image'>
-            <img src={ME} alt='About Image'/>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
           </div>
         </div>
 
-        <div className='about__content'>
-          <div className='about__cards'>
-
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>2+ Years Working</small>
+              <small>4+ Years Working</small>
             </article>
 
-            <article className='about__card'>
-              <LiaLanguageSolid className='about__icon'/>
+            <article className="about__card">
+              <LiaLanguageSolid className="about__icon" />
               <h5>Expertise</h5>
-              <small>7+ Programming Languages</small>
+              <small>10+ Programming Languages</small>
             </article>
 
-            <article className='about__card'>
-              <LuFolderTree className='about__icon'/>
+            <article className="about__card">
+              <LuFolderTree className="about__icon" />
               <h5>Projects</h5>
-              <small>10+ Completed</small>
+              <small>15+ Completed</small>
             </article>
-
           </div>
           <p>
-            I'm a Computer Science and Engineering Undergraduate and an energetic, versatile, and highly dedicated person 
-            who is passionate to do hard work and achieving goals.
+            I'm a Computer Science and Engineering Undergraduate and an
+            energetic, versatile, and highly dedicated person who is passionate
+            to do hard work and achieving goals.
           </p>
 
-          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
-
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
